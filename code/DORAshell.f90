0f4d90b4c71840bfb2ddd07a7c9ce75975f82503
ovasseur <vasseur@ipno.in2p3.fr>
2017-07-13 17:45:35 +0200

    Add files via upload

--- code/DORAshell.f90
+++ code/DORAshell.f90
@@ -9,13 +9,44 @@
 ! ==============================================================================
 
 
+! ==============================================================================
+! GLOBAL VARIABLES THAT ARE NEITHER INPUT NOR PARAMETERS
+! ==============================================================================
+module global
+  
+  use input_DORA
+
+  implicit none
+
+  ! "Include" parameters (maximum values...)
+  include 'param_DORA.f90'
+
+  type sp     ! N.B.: 'structure' and 'record' statements are pre-Fortran 90 statements that are equivalent to 'type' statements, but they are obsolete (not portable). Make sure to always use 'type'.
+     integer :: n     ! Principal quantum number.
+     integer :: l     ! Orbital momentum.
+     real :: j     ! Total angular momentum.
+     real :: mj     ! Total angular momentum projection.
+  end type sp
+  type(sp), dimension(mnsp) :: shell     ! One-dimensional array of sets of quantum numbers (single-particle states).
+!!$  type(sp), dimension(:), allocatable :: shell     ! One-dimensional array of sets of quantum numbers (single-particle states).
+
+!!$  type determinant
+!!$     integer, dimension (A-Z) :: det
+!!$  end type determinant
+  integer, dimension (A-Z) :: slater
+!!$  type(determinant) :: slater
+  integer, dimension (:, :), allocatable :: basis
+
+end module global
+
+
+! ==============================================================================
+! MAIN PROGRAM
+! ==============================================================================
 program DORA
 
   implicit none
   
-  ! "Include" parameters (maximum values...)
-  include 'param_DORA.f90'
-
   ! Test for input file
   call read_input
 
@@ -23,10 +54,10 @@ program DORA
   !!! TO DO !!!
 
   ! Build single-particle states
-  call build_sp_states(mnsp)
+  call build_sp_states
 
-  ! Build Slater determinants
-  call slater
+  ! Build the basis of Slater determinants
+  call build_basis
 
   ! Build Hamiltonian
   !call build_hamiltonian
@@ -44,6 +75,8 @@ subroutine read_input
   
   use input_DORA
 
+  implicit none
+
 !  logical :: file_exists
   integer :: N
 
@@ -52,7 +85,7 @@ subroutine read_input
   N = A-Z
   ! Test
   if ( mod(N+int(2*M),2) .eq. 1 ) then
-     write(6, *) ' ERROR: Inconsistent M input.'
+     write(6, *) " ERROR in subroutine 'input_DORA': Inconsistent M input. Exiting."
      stop
   end if
   write(6, *) 'Z= ', Z
@@ -66,22 +99,14 @@ end subroutine read_input
 ! ==============================================================================
 ! BUILD SINGLE-PARTICLE STATES AND STORE THEM IN A FILE
 ! ==============================================================================
-subroutine build_sp_states(mnsp)
+subroutine build_sp_states
+
+  use global
 
   implicit none
 
-  type sp     ! N.B.: 'structure' and 'record' statements are pre-Fortran 90 statements that are equivalent to 'type' statements, but they are obsolete (not portable). Make sure to always use 'type'.
-     integer :: n     ! Principal quantum number.
-     integer :: l     ! Orbital momentum.
-     real :: j     ! Total angular momentum.
-     real :: mj     ! Total angular momentum projection.
-  end type sp
-
-  type(sp), dimension(mnsp) :: shell     ! One-dimensional array of sets of quantum numbers (single-particle states).
-!!$  type(sp), dimension(:), allocatable :: shell     ! One-dimensional array of sets of quantum numbers (single-particle states).
-
   logical :: flag
-  integer :: mnsp, kn, kl, spi, im     ! spi: single-particle state index in the 'shell' array.
+  integer :: kn, kl, spi, im     ! spi: single-particle state index in the 'shell' array.
   real :: kj, km
 
   open(unit=4, status='unknown', file='sp_states.dat')     ! File where all the single-particle states are stored.
@@ -150,12 +175,61 @@ end subroutine build_sp_states
 ! ==============================================================================
 ! BUILD SLATER DETERMINANTS
 ! ==============================================================================
-subroutine slater(mnsp)
+subroutine build_basis
+
+  use global
+  use input_DORA
   
   implicit none
 
-  use input_DORA
+  character (*), parameter :: fmt = '(i0' // repeat(', 1x, i0', A-Z - 1)//')'
+  integer :: N, k, dim
+  
+  N = A-Z     ! Simple variable change.
 
-  do i = 
+  ! Check if 'basis' is allocated:
+  if ( allocated(basis) ) then
+     write(6, *) " ERROR in subroutine 'build_basis': Array 'basis' is already allocated. Exiting"
+     stop
+  else
+     allocate( basis(dim, N) )
+  end if
 
-end subroutine slater
+  call gen(1)
+
+! ------------------------------------------------------------------------------
+contains
+
+  recursive subroutine gen (c)
+
+    implicit none
+    integer, intent (in) :: c
+    integer :: a, b
+    real :: mtot
+
+    if (c > N) then
+       !write(*, fmt) slater
+       !Test for M
+       mtot = 0e0
+       do b = 1, N
+          mtot = mtot + shell(slater(b))%mj
+       end do
+       ! Check if 'mtot' is compatible with the allowed value 'M'
+       if ( mtot .eq. M ) then
+          write(6, fmt) slater
+          do b = 1, N
+             basis(c, b) = slater(b)
+          end do
+       end if
+    else
+       do a = 1, mnsp
+          if ((c .eq. 1) .or. (a .gt. slater(c - 1))) then
+             slater(c) = a
+             call gen(c + 1)
+          end if
+       end do
+    end if
+    
+  end subroutine gen
+
+end subroutine build_basis
