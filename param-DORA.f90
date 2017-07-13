ed3a9091ffefe8b8e03c42c082c19e6338046d4d
Rosa-Belle <rgerst@ikp.uni-koeln.de>
2017-07-13 20:56:28 +0200

    Delete param-DORA.f90

--- param-DORA.f90
+++
@@ -1,9 +0,0 @@
-! PARAMETERS OF THE SHELL-MODEL CODE
-
-
-  ! Maximum number of single-particle states
-  integer, parameter :: mnsp = 12
-  ! Allowed M value
-  double precision, parameter :: M = 3
-  
- 
