7ca3ba6765a28fa3a91a2ff21ef6cb5fdf0fee8c
Rosa-Belle <rgerst@ikp.uni-koeln.de>
2017-07-20 10:02:54 +0200

    Add files via upload

--- code/sd/input_DORA.f90
+++ code/sd/input_DORA.f90
@@ -1,6 +1,6 @@
 module input_DORA
   implicit none
-  integer, parameter :: NN = 3     ! NN
-  real, parameter    :: M = -1     ! 2*M
+  integer, parameter :: NN = 4     ! NN
+  real, parameter    :: M = -8     ! 2*M
 end module input_DORA
 
